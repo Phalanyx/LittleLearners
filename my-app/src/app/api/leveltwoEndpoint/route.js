c49eaf7ceaa50e3aeaf0fd1fb6b347de6a4b4733
oskip0906 <oscar.pang050906@gmail.com>
2024-10-05 18:50:48 -0400

    Added word bank with images

--- my-app/src/app/api/leveltwoEndpoint/route.js
+++ my-app/src/app/api/leveltwoEndpoint/route.js
@@ -1,35 +1,9 @@
 import { NextResponse } from "next/server";
+import { wordBank } from "./words_to_images.json";
 
 // To handle a GET request to /api
 export async function GET(request) {
-  const wordBank = {
-    'a': 'apple',
-    'b': 'banana',
-    'c': 'carrot',
-    'd': 'dog',
-    'e': 'elephant',
-    'f': 'frog',
-    'g': 'grape',
-    'h': 'hat',
-    'i': 'ice',
-    'j': 'jelly',
-    'k': 'kite',
-    'l': 'lemon',
-    'm': 'monkey',
-    'n': 'nose',
-    'o': 'orange',
-    'p': 'pear',
-    'q': 'queen',
-    'r': 'rabbit',
-    's': 'snake',
-    't': 'tiger',
-    'u': 'umbrella',
-    'v': 'violin',
-    'w': 'whale',
-    'x': 'xylophone',
-    'y': 'yogurt',
-    'z': 'zebra'
-  }
+
   const randomWords = Object.values(wordBank)
     .sort(() => 0.5 - Math.random())
     .slice(0, 3);
@@ -38,4 +12,5 @@ export async function GET(request) {
     "b": randomWords[1],
     "c": randomWords[2]
    }, { status: 200 });
+   
 }
