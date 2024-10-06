a999265226f3d29a857d4ff4f0dc222ad1100c69
Albert Huynh <itsalbert.huynh@mail.utoronto.ca>
2024-10-05 21:04:23 -0400

    fix

--- my-app/src/app/api/leveltwoEndpoint/route.js
+++ my-app/src/app/api/leveltwoEndpoint/route.js
@@ -21,11 +21,6 @@ export async function GET(request) {
   const randomWords = Object.values(wordBank)
     .sort(() => 0.5 - Math.random())
     .slice(0, 3);
-  return NextResponse.json({
-    "a": randomWords[0],
-    "b": randomWords[1],
-    "c": randomWords[2]
-   }, { status: 200 });
 
 
   const randomPairs = getRandomKeys(wordBank);
